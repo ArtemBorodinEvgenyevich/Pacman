05f738fc8340a1f926514fc6cd1cf7852b67d9ad
Lev <lkorasik83@gmail.com>
2020-05-06 12:30:47 +0500

    Add
    - Добавил прототип анимации, возможно, так и надо делать
    - Добавил класс Monster.cs
    - Добавил файл с базовыми спрайтами
    - Вырезал оттуда Блинки

--- Thief-Game/Monster.cs
+++ Thief-Game/Monster.cs
@@ -6,5 +6,47 @@ namespace Thief_Game
 {
     class Monster
     {
+        int x;
+        int y;
+
+        int startX;
+        int startY;
+
+        //Direction - возможно, понадобится для построения траекторий
+
+        Behaviors currentBehavior;
+
+
+        public Monster()
+        {
+
+        }
+        
+        public void MoveUp()
+        {
+
+        }
+
+        public void MoveLeft()
+        {
+
+        }
+
+        public void MoveRight()
+        {
+
+        }
+
+        public void MoveDown()
+        {
+
+        }
+    }
+
+    enum Behaviors
+    {
+        PURSUITING,
+        DISPERSING,
+        FRIGHTNING
     }
 }
---
+++ Thief-Game/Scene.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Thief_Game
+{
+    public partial class Scene : Form
+    {
+        float x = 0f;
+        float y = 0f;
+
+        public Scene()
+        {
+            KeyPress += (sender, e) =>
+            {
+                //MessageBox.Show("Press");
+
+                switch (e.KeyChar)
+                {
+                    case 'w':
+                        y -= 10;
+                        break;
+                    case 's':
+                        y += 10;
+                        break;
+                    case 'a':
+                        x -= 10;
+                        break;
+                    case 'd':
+                        x += 10;
+                        break;
+                }
+
+                Invalidate();
+            };
+
+            Paint += (sender, e) =>
+            {
+                //Image blinky = Image.FromFile(@"C:\C#\Thief-Game\Thief-Game\Source\Blinky.png");
+
+                //var graphic = e.Graphics;
+
+                //graphic.DrawImage(blinky, 10f, 10f);
+            };
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Image blinky = Image.FromFile(@"C:\C#\Thief-Game\Thief-Game\Source\Blinky.png");
+
+            var graphic = e.Graphics;
+
+            graphic.DrawImage(blinky, x, y);
+        }
+    }
+}
