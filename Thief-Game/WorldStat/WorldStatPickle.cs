52fa9f89b502dd70599bb6d0954709217f142b2f
Artem Borodin <temych02@gmail.com>
2020-05-19 15:42:26 +0500

    Update WorldStatPickle.cs

--- Thief-Game/WorldStat/WorldStatPickle.cs
+++ Thief-Game/WorldStat/WorldStatPickle.cs
@@ -15,12 +15,14 @@ namespace Thief_Game
         public WorldStatPickle()
         {
             pathToFile = Path.Combine(PathInfo.WorkingDir, "ScoreRecord.json");
+            
             if (!File.Exists(pathToFile))
             {
-                File.Create(pathToFile);
-                string jsonString = @"{""ScoreTotal"":0,""ScoreRecord"":[11500, 11400]}";
+                File.Create(pathToFile).Close();
+                string jsonString = @"{""ScoreTotal"":0,""ScoreRecord"":[1500, 1400]}";
                 File.WriteAllText(pathToFile, jsonString);
             }
+            
                 
         }
 
